cc076b3209f5a309cf1e08c0f4e110ea73103d28
gracegimon <gracegimon@gmail.com>
2013-02-02 20:58:42 -0430

    Added the signup layout

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -20,4 +20,25 @@ describe "LayoutLinks" do
 		visit help_path
 		page.should have_selector('title', :content => "Help")
 	end
+	
+	it "should have a Signup page at '/signup'" do
+		visit signup_path
+		page.should have_selector('title', :content => "Sign up")
+	end
+
+	it "should have the right links on the layout" do
+		visit root_path
+		page.should have_selector('title', :content => "Home")
+		click_link "About"
+		page.should have_selector('title', :content => "About")
+		click_link "Contact"
+		page.should have_selector('title', :content => "Contact")
+		click_link "Home"
+		page.should have_selector('title', :content => "Home")
+		click_link "Sign up now!"
+		page.should have_selector('title', :content => "Sign up")
+		page.should have_selector('a[href="/"]>img') #We want to check
+													 #the link of the logo
+
+	end
 end
