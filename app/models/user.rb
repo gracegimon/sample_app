c20aa16dff1c7da01d634046a64ccffd922c1503
gracegimon <gracegimon@gmail.com>
2013-02-09 22:48:15 -0430

    Finished password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,17 +2,18 @@
 #
 # Table name: users
 #
-#  id         :integer          not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id                 :integer          not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  encrypted_password :string(255)
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :email, :name 
-  # Just the attributes that the user
-  # can modify
+  attr_accessor :password # Just get and set (user model objects)
+  attr_accessible :email, :name, :password, :password_confirmation
+  # Just the attributes that the real user can modify
 email_regex= /[\w+\-.]+@[a-z\d.]+\.[a-z]+/i #The i means case insensitive
 #Validation for the presence of :name
 validates :name, :presence => true,
@@ -20,6 +21,47 @@ validates :name, :presence => true,
 validates :email, :presence => true,
 				  :format => {with: email_regex},
 				  :uniqueness => {:case_sensitive => false}
+validates :password, :presence => true,
+					 :confirmation => true,
+					 :length => {:within => 6..40}
+#Register a callback
+#Callback: A method that's called in a particular life time of the object
 
+before_save :encrypt_password #Before saving the user, encrypt the password
 
+def has_password?(submitted_password)
+	encrypted_password == encrypt(submitted_password)
 end
+
+
+
+private
+  #Set the encrypted password of the user
+	def encrypt_password
+		self.salt = make_salt if new_record?
+		self.encrypted_password = encrypt(password)
+	end
+
+	def encrypt(string)
+		secure_hash("#{salt}--#{string}") 
+	end
+
+	def secure_hash(string)
+		Digest::SHA2.hexdigest(string)
+	end
+
+	def make_salt
+		secure_hash("#{Time.now.utc}--#{password}")
+	end
+	# class << self 
+	# define class methods
+	# Don't need User.
+	class << self
+	def User.authenticate(email, submitted_password)
+		user = find_by_email(email)
+		return nil if user.nil?
+		return user if user.has_password?(submitted_password)
+	end
+	end
+end
+
