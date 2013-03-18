1a12370c79ecb0c71f27550e2c049e43a1334e4e
gracegimon <gracegimon@gmail.com>
2013-03-17 21:25:38 -0430

    Handling invalid sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+  	user = User.authenticate(params[:session][:email],
+  		params[:session][:password])
+  	if user.nil?
+  		flash.now[:error] = "Invalid email/password combination" #Only persists for this request
+  	 	render 'new' # In case of a fail submit, re-render the sign in page
+  	else
+  		#Handle succesful
+  	end
   end
 
   def destroy
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -3,20 +3,44 @@ require 'spec_helper'
 describe SessionsController do
  render_views
 
-  describe "GET 'new'" do
-    
-    it "returns http success" do
-      get 'new'
-      response.should be_success
-    end
+	describe "GET 'new'" do
 
- #   it "should have the right title" do
- #   	visit sessions_path
- #   	response.should have_selector('title',:content => "Sign in")
- #   end
+		it "returns http success" do
+			get 'new'
+			response.should be_success
+		end
+
+	#   it "should have the right title" do
+	#   	visit sessions_path
+	#   	response.should have_selector('title',:content => "Sign in")
+	#   end
+	end
+
+	describe "POST 'create'" do
+
+		describe "failure" do
+			before(:each) do
+				@attr = {:email=>"", :password => ""}
+			end
+
+			it "should re-render the new page" do
+				post :create, :session => @attr
+				response.should render_template('new')
+			end
+
+			it "should have an error message" do
+				post :create, :session => @attr
+				flash.now[:error].should =~ /invalid/i
+			end
+
+			# it "should have the right title" do
+			# 	post :create, :session =>@attr
+			# 	response.shold have_selector('title', :content =>"Sign in")
+			# end
+
+		end
 
 
-
-  end
+	end
 
 end
