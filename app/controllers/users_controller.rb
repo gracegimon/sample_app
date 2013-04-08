7533943fae1cb5733cccab255dc8409b7efe1ba2
Grace Gimon <gracegimon@gmail.com>
2013-04-08 17:57:27 -0430

    Creating all users interface

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,14 @@
 class UsersController < ApplicationController
   
-  before_filter :authenticate, :only => [:edit, :update]
+  before_filter :authenticate, :only => [ :index, :edit, :update]
    before_filter :correct_user, :only => [:edit, :update]
 
+  def index
+    @users= User.all
+    
+  end
+  
+  
   def new
   	@user = User.new
   	@title = "Sign up"
