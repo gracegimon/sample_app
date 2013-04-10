04b509f613c217e50f9807845a5f5d838ce01169
gracegimon <gracegimon@gmail.com>
2013-04-10 17:23:58 -0430

    Microposts controller

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -22,6 +22,10 @@ module SessionsHelper
  	!current_user.nil?
  end
 
+ def authenticate
+  deny_access unless signed_in?
+ end
+
  def sign_out
  	cookies.delete(:remember_token)
  	current_user = nil
