f3d005f289b3d2a8054f9d69ce26bd639620e8f6
gracegimon <gracegimon@gmail.com>
2013-04-12 13:18:04 -0430

    Functional Follow and unfollow buttons

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,16 +2,24 @@ class RelationshipsController < ApplicationController
 before_filter :authenticate
 
 	def create 
-		user = User.find(params[:relationship][:followed_id])
-		current_user.follow!(user)
-		flash[:success] = "You are following #{user.name}"
-		redirect_to user
+		@user = User.find(params[:relationship][:followed_id])
+		current_user.follow!(@user)
+		flash[:success] = "You are following #{@user.name}"
+		respond_to do |format|
+			format.html{redirect_to @user}
+			format.js
+		end
+		
 	end
 
 	def destroy
-		relationship = Relationship.find(params[:id]).destroy
+		@user = Relationship.find(params[:id]).followed
 		flash[:success] = "Unfollowed"
-		redirect_to relationship.followed
+		current_user.unfollow!(@user)
+		respond_to do |format|
+			format.html{redirect_to @user}
+			format.js
+		end
 	end
 
 end
--- spec/controllers/relationships_controller_spec.rb
+++ spec/controllers/relationships_controller_spec.rb
@@ -23,8 +23,16 @@ describe RelationshipsController do
 		it "should create a relationship" do
 			lambda do
 				post :create, :relationship => {:followed_id => @followed}
-				response.should redirect_to(signin_path)
+				response.should redirect_to(user_path(@followed))
 			end.should change(Relationship, :count).by(1)
+		end
+
+		it "should create a relationship with Ajax" do
+			lambda do
+				xhr :post, :create, :relationship => {:followed_id => @followed}
+				response.should be_success
+			end.should change(Relationship, :count).by(1)
+
 		end
 
 	end
@@ -41,6 +49,13 @@ describe RelationshipsController do
 			lambda do
 				delete :destroy, :id => @relationship
 				response.should redirect_to(user_path(@followed))
+			end.should change(Relationship, :count).by(-1)
+		end
+
+		it "should destroy a relationship with Ajax" do
+			lambda do
+				xhr :delete, :destroy, :id => @relationship
+				response.should be_success
 			end.should change(Relationship, :count).by(-1)
 		end
 
